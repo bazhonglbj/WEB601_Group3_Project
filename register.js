497767fc8dd42a7dd28fe30deacc5ea02df25fdf
bazhonglbj <bazhonglbj@gmail.com>
2023-10-17 09:06:25 -0700

    register form

---
+++ register.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    $('.check-empty').on('focusout', function() {
+        if($(this).val() === '') {
+            $(this).addClass('is-invalid');
+            
+            $(this).siblings('.invalid-feedback').text($(this).data('error'));
+        } else {
+            $(this).removeClass('is-invalid');
+        }
+    });
+
+    $('.check-selection').on('focusout', function() {
+        if ($(this).val() === '') {
+            $(this).addClass('is-invalid');  
+        } else {
+            $(this).removeClass('is-invalid');  
+        }
+    });
+
+    $('#course').change(function() {
+        $('#amount').val("$" + $(this).val());
+    });
+
+    $('#registrationForm').on('submit', function(e) {
+        e.preventDefault();
+        
+        if (this.checkValidity() === false) {
+            e.stopPropagation();
+            $(this).addClass('was-validated');
+            return;
+        }
+
+        alert("Payment Successfully!");
+        this.reset();
+        $(this).removeClass('was-validated');
+    });
+  });
